baa8ab28a932a0302aa618d1f729ed04adf33aaf
Jan-Willem <janwillem.nh@gmail.com>
2015-12-09 14:50:41 +0100

    Nearly implemented pseudocode

---
+++ src/module6/DocumentStore.java
@@ -0,0 +1,27 @@
+package module6;
+
+/**
+ * Created by Jan-Willem on 9-12-2015.
+ */
+public class DocumentStore {
+
+    public DocumentStore(String... classes) {
+
+    }
+
+    public String[] getVocab() {
+        return new String[]{"ayy"};
+    }
+
+    public  int getnr() {
+        return 0;
+    }
+
+    public int countDocsInClass(String cls) {
+        return 0;
+    }
+
+    public int countTokensOfTermInClass(String token, String classValue) {
+        return 0;
+    }
+}
--- src/module6/NaiveBayes.java
+++ src/module6/NaiveBayes.java
@@ -1,12 +1,49 @@
 package module6;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Jan-Willem on 9-12-2015.
  */
 public class NaiveBayes {
 
-    public void train() {
+    private DocumentStore docs;
+    private Map prior = new HashMap<String, Double>();
+    private Map condprob = new HashMap<String, HashMap<String, Double>>();
 
+    public NaiveBayes(DocumentStore ds) {
+        docs = ds;
+    }
+
+    public NaiveBayes(String... classes) {
+        docs = new DocumentStore(classes);
+    }
+
+
+    public void train(String[] classes) {
+
+        //assert classes.length = docs.getnr();
+        String[] vocab = docs.getVocab();
+
+
+
+        double[] priorii = new double[classes.length];
+        double[][] condprobii = new double[vocab.length][classes.length];
+        int n = docs.getnr();
+
+        for (int i = 0; i < classes.length; i++) {
+            int docsInClass = docs.countDocsInClass(classes[i]);
+            prior.put(classes[i], (double) docsInClass/ (double) n);
+            priorii[i] = (double) docsInClass/ (double) n;
+
+            for(String token : vocab) {
+                int tokencount = docs.countTokensOfTermInClass(token, classes[i]);
+                for (int j = 0; j < vocab.length; j++) {
+                    condprobii[j][i] = (double) (tokencount+1) / 1; //TODO
+                }
+            }
+        }
     }
 
     public void estimate() {
