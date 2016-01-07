d97a6b555577f4cd2f2fa6c8fd24a01a04e2124e
JLedelay <JLedelay@users.noreply.github.com>
2016-01-07 01:02:54 +0100

    Errors worden afgevangen
    TODO: implementeren functionaliteit van smoothing etc

--- src/module6/GUI.java
+++ src/module6/GUI.java
@@ -22,18 +22,9 @@ import javax.swing.JSlider;
 import javax.swing.JTextArea;
 
 public class GUI {
-
-	/**
-	 * TODO GUI bestand naar DocumentStore knop (gegeven de klasse) bestand
-	 * estimaten -> dit geeft feedback op een of andere manier en dan dit
-	 * bestand ook naar de documentstore sturen met een menselijk gecorrigeerde
-	 * klasse. Knoppen om de smoothing amountOfCondProbsToUse en minOccurrences
-	 * aan te passen is ook handig. Estimate knop heeft geen functionaliteit
-	 * */
-
-	public static NaiveBayes classifier = new NaiveBayes(1.0, 20, 0.30);
+	public static NaiveBayes classifier = new NaiveBayes(1.0, 2, 0.30);
 	public static DocumentStore docs = classifier.getDocumentStore();
-	
+
 	private static void addComponentsToPane(final Container pane) {
 		final JFileChooser chooser = new JFileChooser();
 		pane.setLayout(new GridBagLayout());
@@ -79,9 +70,9 @@ public class GUI {
 		c.weightx = 1;
 		c.gridx = 0;
 		c.gridy = 2;
-		pane.add(estimateButton, c);		
-		
-		final JLabel estimatedClass = new JLabel("Estimated class: ");
+		pane.add(estimateButton, c);
+
+		final JLabel estimatedClass = new JLabel("Class: ");
 		estimatedClass.setHorizontalAlignment(JLabel.CENTER);
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.gridwidth = 1;
@@ -89,7 +80,7 @@ public class GUI {
 		c.gridx = 0;
 		c.gridy = 4;
 		pane.add(estimatedClass, c);
-		
+
 		final JTextArea estimation = new JTextArea();
 		estimation.setRows(1);
 		c.fill = GridBagConstraints.HORIZONTAL;
@@ -98,7 +89,7 @@ public class GUI {
 		c.gridx = 1;
 		c.gridy = 4;
 		pane.add(estimation, c);
-		
+
 		JButton addButton = new JButton("Add file to DocumentStore");
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.gridwidth = 2;
@@ -106,62 +97,99 @@ public class GUI {
 		c.gridx = 2;
 		c.gridy = 4;
 		pane.add(addButton, c);
-		
+
 		JLabel label1 = new JLabel("amount of condprobs");
 		label1.setHorizontalAlignment(JLabel.CENTER);
 		c.fill = GridBagConstraints.HORIZONTAL;
-		c.gridwidth = 2;
+		c.gridwidth = 1;
 		c.weightx = 0;
 		c.gridx = 0;
 		c.gridy = 5;
 		pane.add(label1, c);
-		
+
 		JLabel label2 = new JLabel("Smoothing");
 		label2.setHorizontalAlignment(JLabel.CENTER);
 		c.fill = GridBagConstraints.HORIZONTAL;
-		c.gridwidth = 2;
+		c.gridwidth = 1;
 		c.weightx = 0;
-		c.gridx = 2;
+		c.gridx = 1;
 		c.gridy = 5;
 		pane.add(label2, c);
 		
+		JLabel label3 = new JLabel("Min Occurences");
+		label3.setHorizontalAlignment(JLabel.CENTER);
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridwidth = 1;
+		c.weightx = 0;
+		c.gridx = 2;
+		c.gridy = 5;
+		pane.add(label3, c);
+
 		JSlider slider1 = new JSlider();
 		slider1.setOrientation(JSlider.HORIZONTAL);
 		slider1.setMinimum(0);
 		slider1.setMaximum(100);
-		slider1.setToolTipText("dinges 1");
 		c.fill = GridBagConstraints.HORIZONTAL;
-		c.gridwidth = 2;
-		c.weightx = 1;
+		c.gridwidth = 1;
+		c.weightx = 0.5;
 		c.gridx = 0;
 		c.gridy = 6;
 		pane.add(slider1, c);
-		
+
 		JSlider slider2 = new JSlider();
 		slider2.setOrientation(JSlider.HORIZONTAL);
 		slider2.setMinimum(0);
 		slider2.setMaximum(100);
-		slider2.setToolTipText("dinges 2");
 		c.fill = GridBagConstraints.HORIZONTAL;
-		c.gridwidth = 2;
-		c.weightx = 1;
-		c.gridx = 2;
+		c.gridwidth = 1;
+		c.weightx = 0.5;
+		c.gridx = 1;
 		c.gridy = 6;
 		pane.add(slider2, c);
+		
+		JSlider slider3 = new JSlider();
+		slider3.setOrientation(JSlider.HORIZONTAL);
+		slider3.setMinimum(0);
+		slider3.setMaximum(100);
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridwidth = 2;
+		c.weightx = 0.5;
+		c.gridx = 2;
+		c.gridy = 6;
+		pane.add(slider3, c);
 
 		addButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				File file = chooser.getSelectedFile();
-				try {
-					docs.addDocument(Utils.readFile(file.getAbsolutePath()), estimation.getText());
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
+				if (file != null) {
+					if (estimation.getText().length() > 0) {
+						try {
+							docs.addDocument(
+									Utils.readFile(file.getAbsolutePath()),
+									estimation.getText());
+							fileName.setText("File added!");
+						} catch (IOException e1) {
+							fileName.setText("An error occured while reading the file.");
+						}
+						chooser.setSelectedFile(null);
+						textContent.setText("");
+						estimation.setText("");
+					} else {
+						JOptionPane.showMessageDialog(pane,
+								"Please fill in a class!",
+								"Error: Missing class!",
+								JOptionPane.ERROR_MESSAGE);
+					}
+				} else {
+					JOptionPane.showMessageDialog(pane,
+							"Please select a file!",
+							"Error: Missing file!",
+							JOptionPane.ERROR_MESSAGE);
 				}
 			}
 		});
-		
+
 		browseButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -195,21 +223,36 @@ public class GUI {
 		estimateButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				classifier.train();
-				File file = chooser.getSelectedFile();
-				String estimated = "";
-				try {
-					estimated = classifier.estimate(Utils.readFile(file.toString()));
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} // input hier de class die estimated is
-				String s = (String) JOptionPane.showInputDialog(pane,
-						"The estimated class is \"" + estimated
-								+ "\". What is the actual class?", "Estimation",
-						JOptionPane.PLAIN_MESSAGE, null, null,
-						estimated);
-				estimation.setText(s);
+				if (docs.getVocab().length != 0) {
+					if (chooser.getSelectedFile() != null) {
+						classifier.train();
+						File file = chooser.getSelectedFile();
+						String estimated = "";
+						try {
+							estimated = classifier.estimate(Utils.readFile(file
+									.toString()));
+						} catch (IOException e1) {
+							System.err.println("Error: File can not be read!");
+							estimated = "ERROR";
+							e1.printStackTrace();
+						}
+						String s = (String) JOptionPane.showInputDialog(pane,
+								"The estimated class is \"" + estimated
+										+ "\". What is the actual class?",
+								"Estimation", JOptionPane.PLAIN_MESSAGE, null,
+								null, estimated);
+						estimation.setText(s);
+					} else {
+						JOptionPane.showMessageDialog(pane,
+								"Please select a file!", "Error: Missing file",
+								JOptionPane.ERROR_MESSAGE);	
+					}
+				} else {
+					JOptionPane.showMessageDialog(pane,
+							"First add a document to the Document Store!",
+							"Error: Empty DocStore",
+							JOptionPane.ERROR_MESSAGE);
+				}
 			}
 		});
 
